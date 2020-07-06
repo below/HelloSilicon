3b999ac883db8bab4a15909fc029cf27e3dd779f
Alexander von Below <Alex@vonBelow.Com>
2020-07-06 23:51:09 +0200

    First version of the new SwiftUI App

---
+++ Chapter 10/ToUpper/Shared/ToUpperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToUpperApp.swift
+//  Shared
+//
+//  Created by Alexander von Below on 06.07.20.
+//
+
+import SwiftUI
+
+@main
+struct ToUpperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
