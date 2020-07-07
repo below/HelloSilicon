49e997f8bf3e1a69ada3a1b0cda3dfbb01d74e78
Alexander von Below <Alex@vonBelow.Com>
2020-07-08 00:18:37 +0200

    upper.s is working

--- Chapter 5/upper.s
+++ Chapter 5/upper.s
@@ -10,9 +10,10 @@
 //
 
 .global _start	            // Provide program starting address to linker
+.p2align 2
 
-_start: LDR	X4, =instr // start of input string
-	LDR	X3, =outstr // address of output string
+_start: ADRP	X4, instr@GOTPAGE // start of input string
+	ADRP	X3, outstr@GOTPAGE // address of output string
 // The loop is until byte pointed to by X1 is non-zero
 loop:	LDRB	W5, [X4], #1	// load character and increment pointer
 // If W5 > 'z' then goto cont
@@ -29,18 +30,18 @@ cont:	// end if
 	B.NE	loop		// loop if character isn't null
 
 // Setup the parameters to print our hex number
-// and then call Linux to do it.
+// and then call the Kernel to do it.
 	MOV	X0, #1	    // 1 = StdOut
-	LDR	X1, =outstr // string to print
+	ADRP	X1, outstr@GOTPAGE // string to print
 	SUB	X2, X3, X1  // get the length by subtracting the pointers
-	MOV	X8, #64	    // linux write system call
-	SVC	0 	    // Call Linux to output the string
+	MOV	X16, #4	    // Unix write system call
+	SVC	#0x80 	    // Call kernel to output the string
 
 // Setup the parameters to exit the program
-// and then call Linux to do it.
-	MOV     X0, #0      // Use 0 return code
-        MOV     X8, #93     // Service code 93 terminates
-        SVC     0           // Call Linux to terminate the program
+// and then call the kernel to do it.
+	mov     X0, #0		// Use 0 return code
+	mov     X16, #1		// System call number 1 terminates this program
+	svc     #0x80		// Call kernel to terminate the program
 
 .data
 instr:  .asciz  "This is our Test String that we will convert.\n"
