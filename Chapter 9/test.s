05dae80bfe0fae25b018c81e09044fd49442a421
Alexander von Below <Alex@vonBelow.Com>
2020-07-26 13:51:30 +0200

    [Chapter 9] _printf now working with two arguments on the stack

--- Chapter 9/test.s
+++ Chapter 9/test.s
@@ -5,18 +5,23 @@
 .align 4
 
 _main:	
-	stp	x29, LR, [sp, #16]!     ; 16-byte Folded Spill
-        adrp  	    X0, ptfStr@PAGE // printf format str
+	// Setup
+	mov	x2, #4711
+	stp	x29, LR, [sp, #48]     ; 16-byte Folded Spill
+	adrp  	    X0, ptfStr@PAGE // printf format str
 	add	X0, X0, ptfStr@PAGEOFF
-	mov	x9, sp	// Move the stack pointer into X9
-	mov	x10, #'b'
-	str	x10, [x9]
+	mov     x9, sp
+	mov	x10, #'2'
+	str     x10, [x9]
+        mov     x10, #4711
+        str     x10, [x9, #8]
+
         bl	    _printf	// call printf
 
 	MOV	X0, #0		// return code
-	ldp	x29, LR, [sp], #16     ; 16-byte Folded Reload
+	ldp	x29, LR, [sp, #48]     ; 16-byte Folded Reload
 	RET
 .data
-ptfStr: .asciz	"X%c\n"
+ptfStr: .asciz	"X%c = %32ld, 0x%016lx\n"
 .align 4
 .text
