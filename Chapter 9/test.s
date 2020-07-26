ba92349aa083b313a62a7b76f6e8a81a4d2280c0
Alexander von Below <Alex@vonBelow.Com>
2020-07-26 13:34:48 +0200

    [Chapter 9] Adding a minimal working sample for calling printf on Darwin

---
+++ Chapter 9/test.s
@@ -0,0 +1,22 @@
+// Test for printf on OS X
+
+
+.global _main	            // Provide program starting address
+.align 4
+
+_main:	
+	stp	x29, LR, [sp, #32]     ; 16-byte Folded Spill
+        adrp  	    X0, ptfStr@PAGE // printf format str
+	add	X0, X0, ptfStr@PAGEOFF
+	mov	x9, sp	// Move the stack pointer into X9
+	mov	x10, #'b'
+	str	x10, [x9]
+        bl	    _printf	// call printf
+
+	MOV	X0, #0		// return code
+	ldp	x29, LR, [sp, #32]     ; 16-byte Folded Reload
+	RET
+.data
+ptfStr: .asciz	"X%c\n"
+.align 4
+.text
