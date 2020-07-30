bfeecf54c56df399172ce5d2b976a262e061ec31
Alexander von Below <Alex@vonBelow.Com>
2020-07-31 01:50:16 +0200

    [Chapter 13] It compiles now … but the output is wrong

--- Chapter 13/matrixmultneon.s
+++ Chapter 13/matrixmultneon.s
@@ -15,42 +15,48 @@
 //	D8 - third column of matrix C
 
 .global main // Provide program starting address to linker
+.align 4
 
 main:	
 	STP	X19, X20, [SP, #-16]!
 	STR	LR, [SP, #-16]!
 
 // load matrix A into Neon registers D0, D1, D2
-	LDR	X0, =A		// Address of A
+	ADRP	X0, A@PAGE	// Address of A
+	ADD	X0, X0, A@PAGEOFF
 	LDP	D0, D1, [X0], #16
 	LDR	D2, [X0]
 
 // load matrix B into Neon registers D3, D4, D5
-	LDR	X0, =B		// Address of B
+	ADRP	X0, B@PAGE	// Address of B
+	ADD	X0, X0, B@PAGEOFF
 	LDP	D3, D4, [X0], #16
 	LDR	D5, [X0]
 
 .macro mulcol ccol bcol
-	MUL	\ccol\().4H, V0.4H, \bcol\().4H[0]
-	MLA	\ccol\().4H, V1.4H, \bcol\().4H[1]
-	MLA	\ccol\().4H, V2.4H, \bcol\().4H[2]
+	MUL	\ccol\().4H, V0.4H, \bcol\().4H
+	MLA	\ccol\().4H, V1.4H, \bcol\().4H
+	MLA	\ccol\().4H, V2.4H, \bcol\().4H
 .endm
 
 	mulcol	V6, V3	// process first column
 	mulcol	V7, V4	// process second column
 	mulcol	V8, V5	// process third column
 
-	LDR	X1, =C		// Address of C
+	ADRP	X1, C@PAGE	// Address of C
+	ADD	X1, X1, C@PAGEOFF
 	STP	D6, D7, [X1], #16
 	STR	D8, [X1]
 
 // Print out matrix C
 // Loop through 3 rows printing 3 cols each time.
 	MOV	W19, #3		// Print 3 rows
-	LDR	X20, =C		// Addr of results matrix
+	ADRP	X20, C@PAGE	// Addr of results matrix
+	ADD	X20, X20, C@PAGEOFF
 printloop:
 		
-	LDR	X0, =prtstr	// printf format string
+	ADRP	X0, prtstr@PAGE	// printf format string
+	ADD	X0, X0, prtstr@PAGEOFF
 // print transpose so matrix is in usual row column order.
 // first ldrh post-indexes by 2 for next row
 // so second ldrh adds 6, so is ahead by 2+6=8=row size
@@ -58,7 +64,11 @@ printloop:
 	LDRH	W1, [X20], #2 	// first element in current row
 	LDRH	W2, [X20,#6]	// second element in current row
 	LDRH	W3, [X20,#14]	// third element in curent row
-	BL	printf		// Call printf
+	STR	W1, [SP, #-32]!	// Push W1 onto the stack, leaving room for W2 and W3
+	STR	W2, [SP, #8]
+	STR	W3, [SP, #16]	// Push X3 onto the stack
+	BL	_printf		// Call printf
+	ADD	SP, SP, #32	// Clean up stack
 	SUBS	W19, W19, #1		// Dec loop counter
 	B.NE	printloop	// If not zero loop
 
