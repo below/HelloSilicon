01f92fabbe259283a4c3903354163b88caca70b8
Alexander von Below <Alex@vonBelow.Com>
2020-09-01 22:26:41 +0200

    [Chapter 10] Cleaned up the flow a bit

--- Chapter 10/ToUpper/Shared/ContentView.swift
+++ Chapter 10/ToUpper/Shared/ContentView.swift
@@ -7,33 +7,39 @@
 
 import SwiftUI
 
+class UpperTextModel: ObservableObject {
+    @Published var input = ""
+    @Published var output = ""
+}
+
 struct ContentView: View {
     
     @State var text: String = ""
+    
+    func toUpper(_ input: String) -> String {
+        var output : [CChar] = Array(repeating: 0, count: 255)
+        
+        let s: String = text
+        mytoupper(s, &output)
+        
+        guard let outputString = String(validatingUTF8: output) else {
+            return "Unable to convert \(input)"
+        }
+        return outputString
+    }
+    
     var body: some View {
+        #if os(macOS)
+        let alignment = TextAlignment.leading
+        #else
+        let alignment = TextAlignment.center
+        #endif
+
         VStack {
-            #if os(macOS)
-            let alignment = TextAlignment.leading
-            #else
-            let alignment = TextAlignment.center
-            #endif
+            TextField("Enter some text here", text: $text).padding(20)
+            Text(toUpper(text))
 
-            TextField("Enter some text here", text: $text).padding(20).multilineTextAlignment(alignment)
-            Button(action: {
-                var output : [CChar] = Array(repeating: 0, count: 255)
-                
-                let s: String = text
-                mytoupper(s, &output)
-                
-                guard let outputString = String(validatingUTF8: output) else {
-                    return
-                }
-                text = outputString
-            }) {
-                Text("To Upper")
-            }
-
-        }.padding()
+        }.multilineTextAlignment(alignment).padding()
     }
 }
 
